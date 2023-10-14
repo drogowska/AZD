c5d8560b9b0df21828629ecf95bd5bcaa7c66063
drogowska <60966644+drogowska@users.noreply.github.com>
2023-10-14 14:44:56 +0200

    added svm

--- zad1/kNN+rf.py
+++ zad1/kNN+rf.py
@@ -4,14 +4,19 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 import pandas as pd
 import numpy as np
-from main import preprocessing0
+from main import preprocessing
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+import warnings
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.svm import SVC
 
+
+warnings.filterwarnings('ignore')
 
 # knn + rf + voting
 
-X_train, X_test, y_train, y_test = preprocessing0()
+X_train, X_test, y_train, y_test = preprocessing()
 scaler = StandardScaler()
 scaler.fit(X_train)
 
@@ -25,6 +30,7 @@ knn_gs.fit(X_train, y_train)
 knn_best = knn_gs.best_estimator_
 print(knn_gs.best_params_)
 y_pred = knn_gs.predict(X_test)
+print(accuracy_score(y_test, y_pred))
 # print(confusion_matrix(y_test, y_pred))
 # print(classification_report(y_test, y_pred))
 
@@ -33,12 +39,33 @@ params_rf = {'n_estimators': [100, 200, 300]}
 rf_gs = GridSearchCV(rf, params_rf, cv=5)
 rf_gs.fit(X_train, y_train)
 rf_best = rf_gs.best_estimator_
+rf_pred = rf_gs.predict(X_test)
 print(rf_gs.best_params_)
+print(accuracy_score(y_test, rf_pred))
 
-ensemble = VotingClassifier(estimators=[('knn', knn_best), ('rf', rf_best)],voting='hard')
+# svm =  make_pipeline(StandardScaler(), SVC(gamma='auto'))
+param_grid = {'C': [0.1, 1, 10, 100, 1000],  
+              'gamma': [1, 0.1, 0.01, 0.001, 0.0001], 
+              'kernel': ['rbf']}  
+svm_gs = GridSearchCV(SVC(), param_grid, cv=5) 
+svm_gs.fit(X_train, y_train)
+svm_best = svm_gs.best_estimator_
+svm_pred = svm_gs.predict(X_test)
+print(svm_best.best_params_)
+print(svm_gs.score(X_test, y_test))
+
+
+
+ensemble = VotingClassifier(estimators=[('knn', knn_best), ('rf', rf_best), ('svm', svm_best)],voting='hard')
 ensemble.fit(X_train, y_train)
 acc = ensemble.score(X_test, y_test)
 pred = ensemble.predict(X_test)
 print(acc)
+
+
+print('knn: {}'.format(knn_best.score(X_test, y_test)))
+print('rf: {}'.format(rf_best.score(X_test, y_test)))
+print('svm: {}'.format(svm_best.score(X_test, y_test)))
+
 print(confusion_matrix(y_test, pred))
 print(classification_report(y_test, pred))
