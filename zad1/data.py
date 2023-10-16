9431bf95832ad97822a7e61477af5e99386fb86e
drogowska <60966644+drogowska@users.noreply.github.com>
2023-10-16 18:21:48 +0200

    data update

--- zad1/data.py
+++ zad1/data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.manifold import TSNE
 
 # wykres rozkładu danych w poszczególnych klasach
 df = pd.read_csv("./zad1/data/emotions.csv")
@@ -26,6 +27,14 @@ positive.loc[2, 'fft_0_a':'fft_749_a'].plot(title='fft of positive columns', col
 negative.loc[0, 'fft_0_a':'fft_749_a'].plot(title='fft of negative columns', color = 'tab:blue', ax=axes[1], linewidth=0.5)
 neutral.loc[1, 'fft_0_a':'fft_749_a'].plot(title='fft of neutral columns', color = 'tab:green', ax=axes[2], linewidth=0.5)
 
+plt.show()
 
 
+# t-SNE Visualization
+tsne = TSNE(n_components=2, random_state=42)
+tsne_results = tsne.fit_transform(df.drop('label', axis=1))
+tsne_df = pd.DataFrame(tsne_results, columns=['Dimension 1', 'Dimension 2'])
+tsne_df['label'] = df['label']
+plt.figure(figsize=(10, 8))
+sns.scatterplot(x='Dimension 1', y='Dimension 2', hue='label', data=tsne_df, palette='viridis')
 plt.show()
